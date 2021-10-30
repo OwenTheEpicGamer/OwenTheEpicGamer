aca9670e56acba42d3395ec263710b67f3685009
OwenTheEpicGamer <88258850+OwenTheEpicGamer@users.noreply.github.com>
2021-10-30 10:27:19 -0400

    Update Number.java

--- Number.java
+++ Number.java
@@ -2,7 +2,7 @@ public class Number {
     private int num;
     //Because the word "double" is a reserved word, you can't have it as a method name
     //Therefore, nothing will compile (E)
-    public void odouble() {
+    public void double() {
         num = num*2;
     }
 
