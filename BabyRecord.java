8e568fb3da18b6e10e0733d21c8b48fc5aac23b5
OwenTheEpicGamer <88258850+OwenTheEpicGamer@users.noreply.github.com>
2021-10-30 10:19:09 -0400

    Revert "Add files via upload"

--- BabyRecord.java
+++
@@ -1,2 +0,0 @@
-//Option E: public BabyRecord(String n, int w, int a)
-//This constructor with the same parameters has been declared already
--- Number.java
+++
@@ -1,9 +0,0 @@
-public class Number {
-    private int num;
-    //Because the word "double" is a reserved word, you can't have it as a method name
-    //Therefore, nothing will compile (E)
-    public void odouble() {
-        num = num*2;
-    }
-
-}
